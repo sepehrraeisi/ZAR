0a0c2f22cd9a86a3bc314b110de73470465ed920
sepehrraeisi <sepehr.raeisi@gmail.com>
2026-08-27 17:57:27 +0330

    Exercise repository-backed default app in widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,14 +1,17 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_app/main.dart';
+import 'package:flutter_app/repository_phase_a2_app.dart';
 
 void main() {
-  testWidgets('ZAR+ renders home shell', (WidgetTester tester) async {
-    await tester.pumpWidget(const ZarPlusApp());
+  testWidgets('repository-backed ZAR+ renders operational shell', (WidgetTester tester) async {
+    await tester.pumpWidget(const RepositoryZarPlusApp());
+    await tester.pumpAndSettle();
 
     expect(find.text('ZAR+'), findsOneWidget);
     expect(find.text('خانه'), findsOneWidget);
     expect(find.text('تقویم'), findsOneWidget);
     expect(find.text('اشخاص'), findsOneWidget);
+    expect(find.text('سوابق'), findsOneWidget);
+    expect(find.text('عقب‌افتاده'), findsOneWidget);
   });
 }
